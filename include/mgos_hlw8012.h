9b72cc328ccbc267e24d0d36a1232518b4a43538
danielbaussou <14319384+danielbaussou@users.noreply.github.com>
2023-09-18 19:58:42 +0200

    Add files via upload

---
+++ include/mgos_hlw8012.h
@@ -0,0 +1,46 @@
+#include "HLW8012.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+HLW8012 *mgos_hlw8012_create();
+
+void mgos_hlw8012_close(HLW8012 *sensor);
+
+bool mgos_hlw8012_begin(HLW8012 *sensor, 
+            unsigned char cf_pin, 
+            unsigned char cf1_pin, 
+            unsigned char sel_pin,
+            unsigned char currentWhen = HIGH,
+            bool use_interrupts = true,
+            unsigned long pulse_timeout = PULSE_TIMEOUT);
+
+double mgos_hlw8012_readCurrent(HLW8012 *sensor);
+
+unsigned int mgos_hlw8012_readVoltage(HLW8012 *sensor);
+
+unsigned long mgos_hlw8012_readEnergy(HLW8012 *sensor);
+
+unsigned int mgos_hlw8012_readActivePower(HLW8012 *sensor);
+        
+unsigned int mgos_hlw8012_readApparentPower(HLW8012 *sensor);
+
+double mgos_hlw8012_readPowerFactor(HLW8012 *sensor);
+
+unsigned int mgos_hlw8012_readReactivePower(HLW8012 *sensor);
+
+void mgos_hlw8012_resetEnergy(HLW8012 *sensor);
+
+void mgos_hlw8012_setCurrentMultiplier(HLW8012 *sensor, double current_multiplier);
+    
+void mgos_hlw8012_setVoltageMultiplier(HLW8012 *sensor, double voltage_multiplier);
+    
+void mgos_hlw8012_setPowerMultiplier(HLW8012 *sensor, double power_multiplier);
+
+void mgos_hlw8012_setResistors(HLW8012 *sensor, double current, double voltage_upstream, double voltage_downstream);
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
